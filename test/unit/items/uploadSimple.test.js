9cf7c6f8ac623e05ce066846f148c7f2479a8469
gogi.strat <gogi.strat@gmail.com>
2016-04-25 07:42:47 -0400

    option to define parentPath in uploadSipmle action

--- test/unit/items/uploadSimple.test.js
+++ test/unit/items/uploadSimple.test.js
@@ -5,13 +5,23 @@ var faker = require('faker'),
 
 describe("uploadSimple", function(){
   
-  var filename, readableStream, fileContent, createdFile;
+  var filename, readableStream, fileContent, createdFile, folderName, createdFolder, createdFile2;
   
-  before(function(){
+  before(function(done){
     
     filename = "test-uploadSimple-" + faker.random.word();
     fileContent = faker.lorem.paragraphs();
     readableStream = new stringStream(fileContent);
+    folderName = "test-uploadSimpleFolder-" + faker.random.word();
+    
+    oneDrive.items.createFolder({
+      accessToken: accessToken,
+      rootItemId: "root",
+      name: folderName
+    }).then(function(folder){
+      createdFolder = folder;
+      done();
+    }).catch(done);
     
   });
   
@@ -21,6 +31,17 @@ describe("uploadSimple", function(){
       accessToken: accessToken,
       itemId: createdFile.id
     }).then(function(_item){
+       return oneDrive.items.delete({
+        accessToken: accessToken,
+        itemId: createdFile2.id
+      });      
+    })
+    .then(function(){
+      return oneDrive.items.delete({
+        accessToken: accessToken,
+        itemId: createdFolder.id
+      });
+    }).then(function(_folder){
       done();
     }).catch(errorHandler(done));
     
@@ -40,6 +61,26 @@ describe("uploadSimple", function(){
       expect(item.size).to.be.at.least(1);
       expect(item.id).to.be.a('String');
       createdFile = item;
+      done();
+    }).catch(errorHandler(done));
+    
+  });
+  
+  it("Should upload Simple file using Stream and parentPath", function(done){
+    
+    oneDrive.items.uploadSimple({
+      accessToken: accessToken,
+      filename: filename,
+      parentPath: folderName,
+      readableStream: readableStream
+    }).then(function(item){
+      
+      expect(item.id).to.be.a('String');
+      expect(item.name).to.be.a('String');
+      expect(item.size).to.be.a('Number');
+      expect(item.size).to.be.at.least(1);
+      expect(item.id).to.be.a('String');
+      createdFile2 = item;
       done();
     }).catch(errorHandler(done));
     
