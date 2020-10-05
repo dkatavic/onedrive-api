253d9ffbe660a54b9cbe842a8919734b6fde6f1a
Do Hoang Nam <do.hoangnam9x@gmail.com>
2020-10-05 17:08:46 +0700

    WIP got for createfolder, delete, download

--- test/e2e/items/download.test.js
+++ test/e2e/items/download.test.js
@@ -1,60 +1,62 @@
 // download.test.js
 
-var faker = require('faker'),
-    stringStream = require('string-to-stream');
+var faker = require("faker"),
+  stringStream = require("string-to-stream");
 
-describe("download", function(){
-
+describe("download", function () {
   var filename, readableStream, fileContent, createdFile;
 
-  before(function(done){
-
+  before(function (done) {
     filename = "test-download-" + faker.random.word();
     fileContent = faker.lorem.paragraphs();
     readableStream = stringStream(fileContent);
 
-    oneDrive.items.uploadSimple({
-      accessToken: accessToken,
-      filename: filename,
-      readableStream: readableStream
-    }).then(function(item){
-      createdFile = item;
-      done();
-    }).catch(errorHandler(done));
-
+    oneDrive.items
+      .uploadSimple({
+        accessToken: accessToken,
+        filename: filename,
+        readableStream: readableStream,
+      })
+      .then(function (item) {
+        createdFile = item;
+        done();
+      })
+      .catch(errorHandler(done));
   });
 
-  after(function(done){
-
-    oneDrive.items.delete({
-      accessToken: accessToken,
-      itemId: createdFile.id
-    }).then(function(_item){
-      done();
-    }).catch(errorHandler(done));
-
-  })
+  after(function (done) {
+    oneDrive.items
+      .delete({
+        accessToken: accessToken,
+        itemId: createdFile.id,
+      })
+      .then(function (_item) {
+        done();
+      })
+      .catch(errorHandler(done));
+  });
 
   it("Should download Simple file using Stream", function (done) {
     var partialString = "";
-    var fileStream = oneDrive.items.download({
-      accessToken: accessToken,
-      itemId: createdFile.id
-    });
+    oneDrive.items
+      .download({
+        accessToken: accessToken,
+        itemId: createdFile.id,
+        progress: (p) => console.log(p),
+      })
+      .then((fileStream) => {
+        fileStream.on("data", function (data) {
+          partialString += data.toString();
+        });
 
-    fileStream.on('data', function(data){
-      partialString += data.toString();
-    });
+        fileStream.on("end", function () {
+          expect(partialString).to.be.equal(fileContent);
+          done();
+        });
 
-    fileStream.on("end", function () {
-      expect(partialString).to.be.equal(fileContent);
-      done();
-    });
-
-    fileStream.on('error', function(err){
-      errorHandler(done)(err);
-    });
-
+        fileStream.on("error", function (err) {
+          errorHandler(done)(err);
+        });
+      });
   });
-
 });
